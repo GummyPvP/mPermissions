a556b249d2578a5beab9a4fd94f60b3eab5839b4
xXkguyXx <jkacuties@gmail.com>
2016-03-29 19:46:30 -0400

    Changed mPermissions2 to mPermissions

---
+++ mPermissions/src/mpermissions/events/PlayerQuit.java
@@ -0,0 +1,46 @@
+package mpermissions.events;
+
+import mpermissions.main.Main;
+import mpermissions.utils.ConfigManager;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.permissions.PermissionAttachment;
+
+public class PlayerQuit implements Listener {
+	
+	@EventHandler
+	public void onQuit(PlayerQuitEvent e) {
+		
+		Player p = e.getPlayer();
+		
+		
+		PermissionAttachment pa = p.addAttachment(Main.getPlugin());
+		
+		
+		for (String perms : ConfigManager.getPermissionsFile().getGroupPermissions(ConfigManager.getPermissionsFile().getGroup(p.getName()))) {
+			
+			pa.unsetPermission(perms);
+		}
+		
+		for (String perms : ConfigManager.getPermissionsFile().getPermissions(p.getName())) {
+			
+			pa.unsetPermission(perms);
+			
+			
+		}
+		
+//		for (String inheritancePermissions : ConfigManager.getPermissionsFile().getInheritancePermissions(ConfigManager.getPermissionsFile().getGroup(p.getName()))) {
+//			
+//			if (inheritancePermissions == null) continue;
+//			
+//			pa.unsetPermission(inheritancePermissions);
+//			
+//		}
+		
+		
+	}
+
+}
